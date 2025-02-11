0a82c28abfe1e7cbc8d883344459b3294662a486
JacopoTrincanato <trincanatoj@gmail.com>
2025-02-11 15:46:43 +0100

    add org.lessons.java package, add main method and print "Hello Java!" message

--- hello-java/src/StampaSaluto.java
+++ hello-java/src/StampaSaluto.java
@@ -1,3 +1,9 @@
 public class StampaSaluto {
 
+    // aggiungo il metodo main
+    public static void main(String[] args) throws Exception {
+
+        // stampo un messaggio di saluto
+        System.out.println("Hello Java!");
+    }
 }
